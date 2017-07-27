570d9e87cbdf7a3ebe2e0ca2839325b0965e9c4b
Calvin <xcalvin.zhao@gmail.com>
2017-07-27 22:53:58 +0800

    step 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727144016) do
+ActiveRecord::Schema.define(version: 20170727145239) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20170727144016) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.boolean  "id_admin",               default: false
+    t.boolean  "is_admin",               default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
